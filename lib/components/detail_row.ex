949490a61e3828d705ceb63f1c2368fe7789f3a6
Matt Hall <401matthall@gmail.com>
2022-08-19 16:34:41 -0400

    Updated DetailRow example
    Updated the detail row example for clarity

--- lib/components/detail_row.ex
+++ lib/components/detail_row.ex
@@ -7,10 +7,10 @@ defmodule Lilly.Components.DetailRow do
 
   def render(assigns) do
     ~F"""
-    <div class={"px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6", is_striped(assigns)}>
-      <dt class="text-sm font-medium text-gray-500">{@label}</dt>
-      <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{@value}</dd>
-    </div>
+      <div class={"px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6", is_striped(assigns)}>
+        <dt class="text-sm font-medium text-gray-500">{@label}</dt>
+        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{@value}</dd>
+      </div>
     """
   end
 
--- priv/catalogue/detail_row/examples.ex
+++ priv/catalogue/detail_row/examples.ex
@@ -1,23 +1,25 @@
 defmodule Lilly.Catalogue.DetailRow.Example01 do
   use Surface.Catalogue.Example,
     subject: Lilly.Components.DetailRow,
-    catalogue: Lilly.Catalogue
+    catalogue: Lilly.Catalogue,
+    height: "320px",
+    direction: "vertical"
 
   def render(assigns) do
     ~F"""
-      <DetailRow label="Author Name" value="Neil Gaiman" />
-    """
-  end
-end
-
-defmodule Lilly.Catalogue.DetailRow.Example02 do
-  use Surface.Catalogue.Example,
-    subject: Lilly.Components.DetailRow,
-    catalogue: Lilly.Catalogue
-
-  def render(assigns) do
-    ~F"""
-      <DetailRow label="Title" value="The Ocean at the End of the Lane" striped={true} />
+    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div class="px-4 py-5 sm:px-6">
+        <h3 class="text-lg leading-6 font-medium text-gray-900">Book Information</h3>
+        <p class="mt-1 max-w-2xl text-sm text-gray-500">Details</p>
+      </div>
+      <div class="border-t border-gray-200">
+        <dl>
+          <DetailRow label="Author Name" value="Neil Gaiman" />
+          <DetailRow label="Title" value="The Ocean at the End of the Lane" striped={true} />
+          <DetailRow label="Published" value="2013" />
+        </dl>
+      </div>
+    </div>
     """
   end
 end
