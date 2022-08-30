8c7db63e65ccb2bcea6956298a1ecf27f1f39f8b
Matt Hall <401matthall@gmail.com>
2022-08-30 18:34:21 -0400

    Added test to prove Grid component grids properly

--- lib/lilly.ex
+++ lib/lilly.ex
@@ -2,17 +2,4 @@ defmodule Lilly do
   @moduledoc """
   Documentation for `Lilly`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Lilly.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/lilly_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule LillyTest do
-  use ExUnit.Case
-  doctest Lilly
-
-  test "greets the world" do
-    assert Lilly.hello() == :world
-  end
-end
